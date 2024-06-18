310ef07b3468f636a3049d116545952f42a0d3d4
Edward Andress <eddie_andress@hotmail.com>
2024-06-18 10:37:55 +0100

    [SLUG] Transferring changes from Slug as of 2024-06-18 10:37:53 +0100.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chapter 1 Challenges
+
+This repo contains the challenges for Chapter 1 of Python Foundations.
+
+## Getting Started
+
+- Fork this repo to your account
+- Clone your forked repo to your machine
+- Refer to the guidance on Journey for further info
---
+++ drills/lib/_2_calling_functions_with_arguments.py
@@ -0,0 +1,92 @@
+# == INSTRUCTIONS ==
+#
+# These challenges are a bit trickier and, in some cases, will require a few
+# lines of code. If you start to get a little stuck, take a step back and make
+# a plan by breaking the overall task down into small steps.
+
+# == EXERCISES ==
+
+# Purpose: checks if a string starts with the letter a
+# Example:
+#   Call:    starts_with_the_letter_a("apple")
+#   Returns: True
+#   Call:    starts_with_the_letter_a("Apple")
+#   Returns: True
+#   Call:    starts_with_the_letter_a("Rock")
+#   Returns: False
+def starts_with_the_letter_a(string):
+    # your code goes here (delete the pass below)
+    pass
+
+
+# Purpose: checks if a string ends with the letter a
+# Example:
+#   Call:    ends_with_the_letter_a("Java")
+#   Returns: True
+#   Call:    ends_with_the_letter_a("JAVA")
+#   Returns: True
+#   Call:    ends_with_the_letter_a("Python")
+#   Returns: False
+def ends_with_the_letter_a(string):
+    # your code goes here (delete the pass below)
+    pass
+
+
+# Purpose: checks if a string contains the word hello
+# Example:
+#   Call:    contains_hello("hello world")
+#   Returns: True
+#   Call:    contains_hello("HELLO WORLD")
+#   Returns: True
+#   Call:    contains_hello("world")
+#   Returns: False
+def contains_hello(string):
+    # your code goes here (delete the pass below)
+    pass
+
+
+# Purpose: replaces the word hello with the word goodbye
+# Note: you don't need to match 'Hello' or 'HELLO' here,
+#       lowercase only is the aim.
+# Example:
+#   Call:    substitute_hello_with_goodbye("hello folks")
+#   Returns: "goodbye folks"
+#   Call:    substitute_hello_with_goodbye("Hello folks")
+#   Returns: "Hello folks"
+def substitute_hello_with_goodbye(string):
+    # your code goes here (delete the pass below)
+    pass
+
+
+# Purpose: removes the letter x from a string
+# Example:
+#   Call:    remove_x("xoxo")
+#   Returns: "oo"
+#   Call:    remove_x("OXO")
+#   Returns: "OO"
+def remove_x(string):
+    # your code goes here (delete the pass below)
+    pass
+
+
+# Purpose: returns the first half of a string
+# Example:
+#   Call:    first_half("coding")
+#   Returns: "cod"
+# Note: you can assume the string will always have an even number of characters
+def first_half(string):
+    # your code goes here (delete the pass below)
+    pass
+
+
+# Purpose: returns the second half of a string
+# Example:
+#   Call:    second_half("coding")
+#   Returns: "ing"
+# Note: you can assume the string will always have an even number of characters
+def second_half(string):
+    # your code goes here (delete the pass below)
+    pass
+
+
+# Congrats, you're done with this file. Move on to the next one.
